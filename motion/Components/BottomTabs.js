d990972b258b3499599c6477b49b1945f548cae1
ConnectionsLLC <0aniketmishra@gmail.com>
2023-04-16 07:37:01 +0000

    Asdf

--- motion/Components/BottomTabs.js
+++ motion/Components/BottomTabs.js
@@ -14,7 +14,7 @@ const BottomTabs = () => {
         <Ionicons name="play-circle-outline" size={26} color="white" style={{marginTop: 12}} />
         <Ionicons name="notifications-outline" size={26} color="white" style={{marginTop: 12}} />
 
-        <Image
+        <Image 
           style={{ width: 28, height: 28, borderRadius: 50, paddingLeft: 4, marginTop: 10 }}
           source={{ uri: 'https://pbs.twimg.com/profile_banners/44196397/1576183471/600x200' }}
         />
